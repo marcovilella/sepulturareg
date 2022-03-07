6ab5f1096b420635b8394ca0e83b37b00a88f11a
kesleyjs <kesleyjs@outlook.com>
2022-03-07 11:21:00 -0300

    Criação de rota para upload de documentos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::post('/upload', [DocumentosController::class, 'store']);
